570c2bfedfe92f09669d4e3dcb5172f2e79e76b4
agastsya <agastsya007@gmail.com>
2023-08-28 21:01:29 +0530

    connected the database to nodejs

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 const express = require("express");
 const path = require("path");
+const mongoose = require("mongoose");
 
 const app = express();
 
-const userInfo = [];
+mongoose
+  .connect("mongodb://127.0.0.1:27017", { dbName: "backend-rep" })
+  .then(() => console.log(`Database Connected `))
+  .catch((e) => console.log(e));
 
+//CREATING OR GENERATING A MONGOOSE MODEL SCHEMA WHICH GIVES TYPES TO THE MONGOOSE MODEL
+const messageSchema = new mongoose.Schema({
+  name: String,
+  email: String,
+});
+
+const Message = mongoose.model("Message", messageSchema);
 // USING MIDDLEWARES
 app.use(express.static(path.join(path.resolve(), "public")));
 app.use(express.urlencoded({ extended: true }));
@@ -15,23 +26,21 @@ app.get("/", (req, res) => {
   res.render("index", { name: "Agastsya" });
 });
 
-app.get("/add", (req, res) => {
-  res.send("nice");
-});
-
 app.get("/users", (req, res) => {
   res.json({
     userInfo,
   });
 });
 
-app.post("/", (req, res) => {
-  userInfo.push({
-    userName: req.body.name,
-    useEmail: req.body.email,
-  });
+app.post("/", async (req, res) => {
+  // const messageData = {
+  //   name: req.body.name,
+  //   email: req.body.email,
+  // };
+  const { name, email } = req.body;
+  await Message.create({ name, email });
   //res.redirect("/success") you will have to create a new route for this and you can render success page there it is also a way
-  res.render("success", { users: userInfo });
+  res.render("success", { users: "Agastsya" });
 });
 
 app.listen(5000, () => {
